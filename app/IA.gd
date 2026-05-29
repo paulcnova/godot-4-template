48236d8a7b9d89530054c912c63eb4ab2de4bf45
paulcnova <fulagann@gmail.com>
2026-05-29 00:35:43 -0700

    feat: Add input action constants

---
+++ app/IA.gd
@@ -0,0 +1,10 @@
+
+class_name IA
+
+#region Properties
+
+#const CATEGORY_ACTION: String = "cat_action";
+const TOOLTIP_INSPECT: String = "ttp_inspect";
+const TOOLTIP_EXIT: String = "ttp_exit";
+
+#endregion Properties
