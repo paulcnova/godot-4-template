61ebdb82fe6271fb83402020cef04b15a79cb77e
paulcnova <fulagann@gmail.com>
2026-05-29 00:35:24 -0700

    feat: Add layers enumerations

---
+++ game/Layers.gd
@@ -0,0 +1,26 @@
+
+class_name Layers
+
+enum Physics3D {
+	None = 0,
+}
+
+enum Physics2D {
+	None = 0,
+}
+
+enum Render3D {
+	None = 0,
+}
+
+enum Render2D {
+	None = 0,
+}
+
+enum Navigation3D {
+	None = 0,
+}
+
+enum Navigation2D {
+	None = 0,
+}
